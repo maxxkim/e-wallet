7fa369bd7e4a6299ac1fd3601c5a3fb78d3bda04
Max Kim <mkim@stratechcorp.com>
2024-10-28 17:13:01 +0300

    feat(auth): New API integration for domain and data layers

--- lib/data/repository/auth/auth_data_repository.dart
+++ lib/data/repository/auth/auth_data_repository.dart
@@ -1,5 +1,7 @@
 
 import 'package:zippy/data/api/api_util.dart';
+import 'package:zippy/domain/model/auth/auth_inititate_model.dart';
+import 'package:zippy/domain/model/auth/auth_verify_model.dart';
 import 'package:zippy/domain/repository/auth/auth_repository.dart';
 
 class AuthDataRepository extends AuthRepository {
@@ -7,4 +9,15 @@ class AuthDataRepository extends AuthRepository {
 
   AuthDataRepository(this._apiUtil);
 
+  @override
+  Future<AuthInitiate> initiateAuth() {
+    return _apiUtil.initiateAuth();
+  }
+
+  @override
+  Future<AuthVerify> verifyAuth() {
+  return _apiUtil.verifyAuth();
+  }
+
+  
 }
--- lib/domain/repository/auth/auth_repository.dart
+++ lib/domain/repository/auth/auth_repository.dart
@@ -1,1 +1,7 @@
-abstract class AuthRepository {}
+import 'package:zippy/domain/model/auth/auth_inititate_model.dart';
+import 'package:zippy/domain/model/auth/auth_verify_model.dart';
+
+abstract class AuthRepository {
+  Future<AuthInitiate> initiateAuth();
+  Future<AuthVerify> verifyAuth();
+}
